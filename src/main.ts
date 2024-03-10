d82a79f042fc5db175b402ded18947f47c66a3cc
User <bruksbond@gmail.com>
2024-03-10 18:48:24 +0200

    add config and edpoint get-all-users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
+import { ConfigService } from "@nestjs/config";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.init();
+  const config = app.get(ConfigService);
+  const port = config.get('port');
+  await app.listen(port);
 }
 bootstrap();
