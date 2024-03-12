d7d77a7f081fd899988556372971eb425b97e19d
bruksbondd <bruksbond@gmail.com>
2024-03-12 17:30:10 +0200

    add guards pasport-jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { TokenModule } from '../token/token.module';
+import { JwtStrategy } from 'src/strategy';
 
 @Module({
   imports: [UsersModule, TokenModule],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
